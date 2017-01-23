459009563c7b690a79497068a50fdab3e8c63109
Derrick Chan <dachanman91@gmail.com>
2017-01-23 15:04:39 -0800

    toggle constructed and arena lists on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+// import React from 'react';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -5,6 +7,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <h1>HS Deck Master</h1>
+        <Nav></Nav>
         <div className="col-md-7">
         </div>
         <div className="col-md-5">
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,28 @@
-var Nav = (props) => (
+var Nav = () => (
   <nav className="navbar">
-    <div className="col-md-6 col-md-offset-3">
-      <Search searching={props.query}/>
+    <button id="constructed-button" onClick={ () => { $('.constructed-list').toggle(); }}>Constructed</button>
+    <div className="constructed-list">
+      <div onClick={() => { console.log('clicked!'); } }>
+        Con Deck 1
+      </div>
+      <div onClick={() => { console.log('clicked!'); } }>
+        Con Deck 2
+      </div>
     </div>
+    <button id="arena-button" onClick={ () => { $('.arena-list').toggle(); }}>Arena</button>
+    <div className="arena-list">
+      <div onClick={() => { console.log('clicked!'); } }>
+        Arena Deck 1
+      </div>
+      <div onClick={() => { console.log('clicked!'); } }>
+        Arena Deck 2
+      </div>
+    </div>
+    <div className="col-md-6 col-md-offset-3"></div>
   </nav>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
+// <Search searching={props.query}/>
 window.Nav = Nav;
