48245e0d16d8884b6504c619cd3de43d84aec935
Derrick Chan <dachanman91@gmail.com>
2017-01-23 12:14:44 -0800

    Setup React and compiler

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="col-md-7">
+        </div>
+        <div className="col-md-5">
+        </div>
+      </div>
+    );
+  }
+}
+
+window.app = app;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+var Nav = (props) => (
+  <nav className="navbar">
+    <div className="col-md-6 col-md-offset-3">
+      <Search searching={props.query}/>
+    </div>
+  </nav>
+);
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.Nav = Nav;
